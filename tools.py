de3fe8e893a9f64daf50edc8834c0bc368b438a9
stelee410 <129747297+stelee410@users.noreply.github.com>
2023-09-20 16:25:13 +0800

    update tools

--- tools.py
+++ tools.py
@@ -1,6 +1,7 @@
 
 from utils import config, model_repos, password_hash
 from sqlalchemy import create_engine
+import random
 
 engine = create_engine(config.connection_str)
 
@@ -16,6 +17,22 @@ def create_user():
         user_repo.insert_user(username, display_name, password_hashed, avatar)
     else:
         print('user existed')
+def reset_password():
+    username = input("Enter your name: ")
+    user_repo = model_repos.UserRepo(engine)
+    user = user_repo.get_user_by_username(username)
+    if user is None:
+        print("user not found")
+        return
+    else:
+        random_number = random.randint(1000000000, 9999999999)
+        password = f"{random_number}"
+        new_password = input(f"Enter your new password({password}): ")
+        if new_password == "":
+            new_password = password
+        password_hashed = password_hash.get_password_hash(new_password, config.secret_key)
+        user_repo.update_password(username, user.password,password_hashed)
+
 def create_invitation_code():
     user_repo = model_repos.UserRepo(engine)
     username = input("Enter your name: ")
@@ -35,12 +52,14 @@ def create_invitation_code():
 
 if __name__ == '__main__':
     while True:
-        action = input("what do you want? create [u]ser | create [i]nvitation code |[q]uit: ")
+        action = input("what do you want? create [u]ser | create [i]nvitation code |[r]eset password| [q]uit: ")
         if action == "u":
             create_user()
         elif action == "i":
             create_invitation_code()
         elif action == "q":
             break
+        elif action == "r":
+            reset_password()
         else:
             print("invalid action")
