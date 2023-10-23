39d53fc2d6bba92421b096d369b30e11e166a8e6
stelee410 <129747297+stelee410@users.noreply.github.com>
2023-10-24 00:31:49 +0800

    sqlite issue

--- tools.py
+++ tools.py
@@ -1,4 +1,6 @@
-
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 from utils import config, model_repos, password_hash
 from sqlalchemy import create_engine
 import random
